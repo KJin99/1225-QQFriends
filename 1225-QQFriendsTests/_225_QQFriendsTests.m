8688bb4fe45dadf26804b741026b6fad2c82b289
KJin99 <kouhanjin@kj.local>
2015-12-25 10:23:11 +0800

    Initial Commit

---
+++ 1225-QQFriendsTests/_225_QQFriendsTests.m
@@ -0,0 +1,39 @@
+//
+//  _225_QQFriendsTests.m
+//  1225-QQFriendsTests
+//
+//  Created by kouhanjin on 15/12/25.
+//  Copyright © 2015年 khj. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _225_QQFriendsTests : XCTestCase
+
+@end
+
+@implementation _225_QQFriendsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
